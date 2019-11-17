4759c10d96db5ecbb9d60e05418bd86b95aa4908
Drew Lockliear <drewlockliear@EMILYs-MBP.localdomain>
2019-11-17 16:59:40 -0600

    Completed gatsby tutorial part-three

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default () => (
+    <Layout>
+        <div>
+            <h1>I'd love to talk, email me at the address below!</h1>
+            <p>
+                <a href="mailto:drew@viws.io">drew@viws.io</a>
+            </p>
+        </div>
+    </Layout>
+)
